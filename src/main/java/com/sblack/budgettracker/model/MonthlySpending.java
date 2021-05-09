cd54e78ee482463044e8bb5713b31db80de60b6d
Sean Black <black.sean14@gmail.com>
2021-05-09 15:52:36 -0700

    New API methods: list-all, search, set default budget, get default budget, get new budget from default

--- src/main/java/com/sblack/budgettracker/model/MonthlySpending.java
+++ src/main/java/com/sblack/budgettracker/model/MonthlySpending.java
@@ -13,6 +13,7 @@ import java.util.Date;
 @Setter
 @Builder
 @ToString
+@With
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
@@ -26,32 +27,32 @@ public class MonthlySpending {
     private YearMonth date;
     private boolean isDefault;
     private boolean inProgress;
-    private float rent;
-    private float groceries;
-    private float travel;
-    private float restaurants;
-    private float loans;
-    private float clothing;
-    private float general_merchandise;
-    private float home_improvement;
-    private float hobbies;
-    private float cable;
-    private float entertainment;
-    private float gifts;
-    private float utilities;
-    private float atm;
-    private float gas;
-    private float healthcare;
-    private float subscriptions;
-    private float gym;
-    private float automotive;
-    private float food_delivery;
-    private float taxes;
-    private float transportation;
-    private float uber;
-    private float charitable_giving;
-    private float mortgage;
-    private float total;
+    private Float rent;
+    private Float groceries;
+    private Float travel;
+    private Float restaurants;
+    private Float loans;
+    private Float clothing;
+    private Float general_merchandise;
+    private Float home_improvement;
+    private Float hobbies;
+    private Float cable;
+    private Float entertainment;
+    private Float gifts;
+    private Float utilities;
+    private Float atm;
+    private Float gas;
+    private Float healthcare;
+    private Float subscriptions;
+    private Float gym;
+    private Float automotive;
+    private Float food_delivery;
+    private Float taxes;
+    private Float transportation;
+    private Float uber;
+    private Float charitable_giving;
+    private Float mortgage;
+    private Float total;
 
     public MonthlySpending calculateTotal() {
         this.total = this.rent + this.groceries + this.travel + this.restaurants + this.loans + this.clothing +
@@ -59,6 +60,35 @@ public class MonthlySpending {
                 this.gifts + this.utilities + this.atm + this.gas + this.healthcare + this.subscriptions +
                 this.uber + this.food_delivery + this.charitable_giving + this.transportation + this.gym + this.taxes +
                 this.automotive + this.mortgage;
+        return this;
+    }
+
+    public MonthlySpending setNullsToZero() {
+        rent = rent == null ? 0F : rent;
+        groceries = groceries == null ? 0F : groceries;
+        travel = travel == null ? 0F : travel;
+        restaurants = restaurants == null ? 0F : restaurants;
+        loans = loans == null ? 0F : loans;
+        clothing = clothing == null ? 0F : clothing;
+        general_merchandise = general_merchandise == null ? 0F : general_merchandise;
+        home_improvement = home_improvement == null ? 0F : home_improvement;
+        hobbies = hobbies == null ? 0F : hobbies;
+        cable = cable == null ? 0F : cable;
+        entertainment = entertainment == null ? 0F : entertainment;
+        gifts = gifts == null ? 0F : gifts;
+        utilities = utilities == null ? 0F : utilities;
+        atm = atm == null ? 0F : atm;
+        gas = gas == null ? 0F : gas;
+        healthcare = healthcare == null ? 0F : healthcare;
+        subscriptions = subscriptions == null ? 0F : subscriptions;
+        uber = uber == null ? 0F : uber;
+        food_delivery = food_delivery == null ? 0F : food_delivery;
+        charitable_giving = charitable_giving == null ? 0F : charitable_giving;
+        transportation = transportation == null ? 0F : transportation;
+        gym = gym == null ? 0F : gym;
+        taxes = taxes == null ? 0F : taxes;
+        automotive = automotive == null ? 0F : automotive;
+        mortgage = mortgage == null ? 0F : mortgage;
         return this;
     }
 }
