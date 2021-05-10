3ee8128afeafb444d1449521e047987d303331fb
Sean Black <black.sean14@gmail.com>
2021-05-09 18:11:28 -0700

    Update API methods: default budget get/set/create, upload-transactions, search, list-all

--- src/main/java/com/sblack/budgettracker/controller/MonthlyTransactionsController.java
+++ src/main/java/com/sblack/budgettracker/controller/MonthlyTransactionsController.java
@@ -6,16 +6,26 @@ import com.sblack.budgettracker.model.MonthlySpending;
 import com.sblack.budgettracker.repositories.MonthlyTransactionsRepository;
 import com.sblack.budgettracker.services.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.YearMonth;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/transactions")
 public class MonthlyTransactionsController {
+
+    private static final YearMonth DEFAULT_DATE = YearMonth.of(0, 1);
+    private static final String SUCCESS = "Success";
+    private static final String DEFAULT_ERROR_MSG = "User cannot add items with 'isDefault=true'.  Please use the Post /default API.";
+
     @Autowired
     private MonthlyTransactionsRepository transactionsRepo;
 
@@ -24,37 +34,55 @@ public class MonthlyTransactionsController {
 
     @GetMapping
     public List<MonthlySpending> list() {
+        List<MonthlySpending> transactionList = transactionsRepo.findAll();
+        return transactionList.stream().filter(item -> !item.isDefault() || item.getType().equals(BudgetType.TEST)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/list-all")
+    public List<MonthlySpending> listAll() {
         return transactionsRepo.findAll();
     }
 
+    @GetMapping("/{id}")
+    public MonthlySpending get(@PathVariable long id) {
+        return transactionsRepo.getOne(id);
+    }
+
     @PostMapping
-    @ResponseStatus(HttpStatus.OK)
-    public void create(@RequestBody MonthlySpending transactions) {
+    public ResponseEntity<String> create(@RequestBody MonthlySpending transactions) {
+        if (transactions.isDefault()) {
+            return new ResponseEntity<>(DEFAULT_ERROR_MSG, HttpStatus.BAD_REQUEST);
+        }
         transactions.calculateTotal();
         transactionsRepo.save(transactions);
+        return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
     }
 
-    // Delete
-    @PostMapping("/total")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> calculateTotals() {
-        List<MonthlySpending> allMonthlySpendings = transactionsRepo.findAll();
-        for( MonthlySpending monthlySpending : allMonthlySpendings) {
-            monthlySpending.calculateTotal();
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update(@PathVariable long id, @RequestBody MonthlySpending monthlySpending) {
+        if (monthlySpending.isDefault()) {
+            return new ResponseEntity<>(DEFAULT_ERROR_MSG, HttpStatus.BAD_REQUEST);
         }
-        transactionsRepo.saveAll(allMonthlySpendings);
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+        monthlySpending.setId(id);
+        transactionsRepo.save(monthlySpending);
+        return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
     }
 
-    @PostMapping("/parse-csv/{source}")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable long id) {
+        transactionsRepo.deleteById(id);
+        return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
+    }
+
+    @PostMapping("/upload-transactions")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> parseCsv(@PathVariable String source,
+    public ResponseEntity<String> uploadTransactions(@RequestParam String source,
                                            @RequestParam("file") MultipartFile file,
                                            @RequestParam("type") String type,
                                            @RequestParam("inProgress") boolean inProgress) {
         try {
             MonthlySpending monthlySpending = fileUploadService.readTransactions(file, source);
-            monthlySpending.setType(BudgetType.valueOf(type));
+            monthlySpending.setType(BudgetType.valueOf(type.toUpperCase()));
             monthlySpending.setInProgress(inProgress);
             monthlySpending.setDefault(false);
             transactionsRepo.save(monthlySpending);
@@ -64,32 +92,60 @@ public class MonthlyTransactionsController {
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
-    public MonthlySpending get(@PathVariable long id) {
-        return transactionsRepo.getOne(id);
-    }
-
-    // Delete
-    @PutMapping("/modify/{id}")
-    public ResponseEntity<String> modify(@PathVariable long id, @RequestParam(required = false) Float rent) {
-        MonthlySpending fromDB = transactionsRepo.getOne(id);
-        if (rent != null) {
-            fromDB.setRent(rent);
+    @PostMapping("/default-budget")
+    @ResponseStatus(HttpStatus.OK)
+    public void createDefaultBudget(@RequestBody MonthlySpending transactions) {
+        // Delete any existing entries.
+        MonthlySpending defaultBudgetExample = MonthlySpending.builder()
+                .isDefault(true)
+                .date(DEFAULT_DATE)
+                .build();
+        List<MonthlySpending> results = transactionsRepo.findAll(Example.of(defaultBudgetExample));
+        for (MonthlySpending result: results) {
+            transactionsRepo.deleteById(result.getId());
         }
-        transactionsRepo.save(fromDB);
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+
+        transactions.calculateTotal();
+        transactions.setDefault(true);
+        transactions.setType(BudgetType.BUDGET);
+        transactions.setDate(DEFAULT_DATE);
+        transactionsRepo.save(transactions);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<String> update(@PathVariable long id, @RequestBody MonthlySpending monthlySpending) {
-        monthlySpending.setId(id);
-        transactionsRepo.save(monthlySpending);
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+    @GetMapping("/default-budget")
+    public MonthlySpending getDefaultBudget() {
+        ExampleMatcher matcher = ExampleMatcher.matchingAll().withIgnoreNullValues();
+        MonthlySpending defaultBudgetExample = MonthlySpending.builder()
+                .isDefault(true)
+                .date(DEFAULT_DATE)
+                .type(BudgetType.BUDGET)
+                .build();
+        Optional<MonthlySpending> result = transactionsRepo.findOne(Example.of(defaultBudgetExample, matcher));
+        return result.isPresent() ? result.get() : null;
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable long id) {
-        transactionsRepo.deleteById(id);
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+    // date of format yyyy-mm
+    @PostMapping("/default-budget/{date}")
+    public void newBudgetFromDefault(@PathVariable String date, @RequestParam boolean inProgress) {
+        YearMonth parsedDate = YearMonth.parse(date);
+        MonthlySpending defaultBudget = this.getDefaultBudget();
+        MonthlySpending newBudgetFromDefault = defaultBudget.withId(null);
+        newBudgetFromDefault.setDefault(false);
+        newBudgetFromDefault.setInProgress(inProgress);
+        newBudgetFromDefault.setDate(parsedDate);
+        newBudgetFromDefault.setId(null);
+        transactionsRepo.save(newBudgetFromDefault);
+    }
+
+    // Search API
+    @GetMapping("/search/{type}/{date}")
+    public List<MonthlySpending> searchForMonthlyTransactions(@PathVariable String type, @PathVariable String date) {
+        MonthlySpending defaultBudgetExample = MonthlySpending.builder()
+                .date(YearMonth.parse(date))
+                .type(BudgetType.valueOf(type.toUpperCase()))
+                .isDefault(false)
+                .build();
+        return transactionsRepo.findAll(Example.of(defaultBudgetExample));
     }
 }
+
